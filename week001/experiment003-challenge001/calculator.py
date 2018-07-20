78df68ccb21a8a5d424e6096892924d099b29c81
lost <lostcode@qq.com>
2018-07-20 14:37:56 +0800

    alter challenge001

--- week001/experiment003-challenge001/calculator.py
+++ week001/experiment003-challenge001/calculator.py
@@ -7,18 +7,22 @@ tax = 0.00
 
 TAX_START_POINT = 3500
 
-# use namedtuple make tax_quick_query_table
-TAX_QUICK_QUERY_TABLE = namedtuple('TAX_QUICK_QUERY_TABLE', [
-	'PAY_FOR_TAX',
-	'TAX_RATE',
-	'QUICK_CALCULATE_NUMBER'
+# use namedtuple make tax_query_table
+quick_lookup_item = namedtuple('quick_lookup_item', [
+	'pay_for_tax',
+	'tax_rate',
+	'quick_calculate_number'
 ])
 
-qqtat = TAX_QUICK_QUERY_TABLE(
-	PAY_FOR_TAX=[80000, 55000, 35000, 9000, 4500, 1500, 0],
-	TAX_RATE=[0.45, 0.35, 0.30, 0.25, 0.20, 0.10, 0.03],
-	QUICK_CALCULATE_NUMBER=[13505, 5505, 2755, 1005, 555, 105, 0]
-)
+tax_query_table = [ 
+	quick_lookup_item(80000, 0.45, 13505),
+	quick_lookup_item(55000, 0.35, 5505),
+	quick_lookup_item(35000, 0.30, 2755),
+	quick_lookup_item(9000, 0.25, 1005),
+	quick_lookup_item(4500, 0.20, 555),
+	quick_lookup_item(1500, 0.10, 105),
+	quick_lookup_item(0, 0.03, 0)
+]
 
 # dispose exceptions
 try: 
@@ -37,8 +41,8 @@ if salary < 3500:
 # if salary equal or greater than TAX_START_POINT
 pay_for_tax = salary - 3500
 
-for shuld_pay in qqtat.PAY_FOR_TAX:
-	if shuld_pay < pay_for_tax:
-		tax = pay_for_tax*qqtat.TAX_RATE[qqtat.PAY_FOR_TAX.index(shuld_pay)] - qqtat.QUICK_CALCULATE_NUMBER[qqtat.PAY_FOR_TAX.index(shuld_pay)]
+for item in tax_query_table:
+	if item.pay_for_tax < pay_for_tax:
+		tax = pay_for_tax * item.tax_rate - item.quick_calculate_number
 		break;	
 print('{:.2f}'.format(tax))
